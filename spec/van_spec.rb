dcf0e02ad8b2f328ab8f2b9b147ca1a0b74245c4
Yvette Cook <yvettecook@gmail.com>
2014-09-18 20:10:52 +0100

    Version 2.7 - Test for overfilling

--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -40,5 +40,13 @@ describe Van do
 		16.times { van.dock(bike) }
 		expect(lambda {van.full?}).to raise_error(RuntimeError)
 	end
+	
+	it "should transfer bikes to station until full, then raise error" do
+		30.times { van.dock(bike) }
+		van.transfer(garage).to raise_error(RuntimeError)
+		expect(van.bike_count).to eq(15)
+		expect(station.bike_count).to eq(15)
+	end
+	
 
 end
